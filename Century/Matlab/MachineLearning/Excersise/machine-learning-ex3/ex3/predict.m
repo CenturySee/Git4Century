f56f97279dbbc000688b11097053490163b5e76d
Century <seecentury@hotmail.com>
2015-08-11 23:30:59 +0800

    Machine Learning Course of Andrew Ng on Coursera Excersise 3 Multi-Class Classification with Logistic Regression and Neural Network 1

---
+++ Century/Matlab/MachineLearning/Excersise/machine-learning-ex3/ex3/predict.m
@@ -0,0 +1,37 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+X0 = [ones(m,1) X];
+% Obtain the Hidden Layer
+aHid = sigmoid(Theta1*X0');
+X1= [ones(1,size(aHid,2)); aHid];
+y = sigmoid(Theta2*X1);
+[~, p] = max(y,[],1);
+p = p';
+
+
+
+
+% =========================================================================
+
+
+end
